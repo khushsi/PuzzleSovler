b7b86a611f8410a54824c3d057c7eb5da811f1f0
khushsi <khushboo.a.singhal@gmail.com>
2016-09-25 14:04:25 -0400

    script

---
+++ PuzzleSolver/run.sh
@@ -0,0 +1,4 @@
+script -c "python PuzzleSolver.py input.config bfs"
+mv typescript typescript.input.config.bfs
+script -c "python PuzzleSolver.py input.config dfs"
+mv typescript typescript.input.config.dfs
